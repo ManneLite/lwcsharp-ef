376a466ee828778a08e57e972e76f5acf2b34bb7
lwcsharp <lwcsharp@gmail.com>
2024-03-04 17:04:59 +0100

    test

--- DataAcess.cs
+++ DataAcess.cs
@@ -1,9 +1,5 @@
 ﻿using EF.Models;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.DependencyInjection;
-using System;
-using System.Configuration;
 
 namespace EF;
 
